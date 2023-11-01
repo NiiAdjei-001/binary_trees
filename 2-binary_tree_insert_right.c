5587d2864251e75ec26eba4441c37bc821c6f7d2
niialabi <taufiq.alabi@gmail.com>
2023-11-01 19:33:42 +0000

    binary tree insert right - clean up

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,14 +12,14 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	binary_tree_t *temp;
 
+	if (!parent)
+		return (NULL);
 	new = binary_tree_node(parent, value);
 	if (parent->right != NULL)
 	{
-		temp = parent->right;
+		new->right = parent->right;
 		parent->right = new;
-		new->right = temp;
 		return (new);
 	}
 	parent->right = new;
