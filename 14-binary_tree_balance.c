f8da654eb86d3e3670f88aaf566cd385936b57d1
niialabi <taufiq.alabi@gmail.com>
2023-11-02 20:17:01 +0000

    tree balance factor task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+size_t bt_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_balance - func measures balance factor of tree
+ * @tree: pointer to root node of a tree
+ * Return: (int) balance factor
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	int bin_tree_left;
+	int bin_tree_right;
+
+	bin_tree_left = bt_height(tree->left);
+	bin_tree_right = bt_height(tree->right);
+	return (bin_tree_left - bin_tree_right);
+}
+
+/**
+ * bt_height - custom binary tree heigh function
+ * @tree: pointer to root
+ * Return: height of tree
+*/
+size_t bt_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = bt_height(tree->left);
+	size_t right_height = bt_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
