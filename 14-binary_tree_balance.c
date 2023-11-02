29be2014183f26272294f53de838be5bc25a71c5
niialabi <taufiq.alabi@gmail.com>
2023-11-02 22:52:50 +0000

    bt balance refactor height function cleanup

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - function to return height of tree
+ * @tree: pointer to node
+ *
+ * Return: dept of node (size_t)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t count = 0;
+	size_t height = 0;
+
+	height_tracer(tree, tree, &count, &height);
+	return (height);
+}
+
+
+/**
  * binary_tree_balance - func measures balance factor of tree
  * @tree: pointer to root node of a tree
  * Return: (int) balance factor
@@ -13,23 +29,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int bin_tree_left;
 	int bin_tree_right;
 
-	bin_tree_left = bt_height(tree->left);
-	bin_tree_right = bt_height(tree->right);
+	bin_tree_left = binary_tree_height(tree->left);
+	bin_tree_right = binary_tree_height(tree->right);
 	return (bin_tree_left - bin_tree_right);
-}
-
-/**
- * bt_height - custom binary tree heigh function
- * @tree: pointer to root
- * Return: height of tree
-*/
-size_t bt_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	size_t left_height = bt_height(tree->left);
-	size_t right_height = bt_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
 }
